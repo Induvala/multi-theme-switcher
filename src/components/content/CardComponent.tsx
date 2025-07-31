f7859e688e9192135e90b7d6933756ee3f65df73
Indu <induvala305@gmail.com>
2025-07-31 17:00:29 +0530

    header and card components

--- src/components/content/CardComponent.tsx
+++ src/components/content/CardComponent.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const CardComponent = () => {
+interface CardProps {
+    image: string;
+    title: string;
+    price: number;
+}
+const CardComponent:React.FC<CardProps> = ( {image, title, price}) => {
   return (
-    <div>CardComponent</div>
+    <div style={{
+        backgroundColor:'#fff',
+        borderRadius:'8px',
+        padding: '10px',
+        boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        transition: 'transform 0.2s',
+    }}>
+        <img src={image} alt={title} style={{width:'100%'}}/>
+          <h3 style={{ fontSize: '1.1rem', margin: '0 0 10px' }}>{title}</h3>
+          <p style={{ fontWeight: 'bold', fontSize: '1rem' }}>${price.toFixed(2)}</p>
+    </div>
   )
 }
 
